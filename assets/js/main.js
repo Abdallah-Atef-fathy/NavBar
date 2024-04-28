e6ae17e168434b5940a95d5b1f5d8aaf7a4ec096
Abdallah Atef <148006055+Abdallah-Atef-fathy@users.noreply.github.com>
2024-04-28 14:19:04 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Varibals
+
+let btn = document.querySelector(".bar");
+let links = document.querySelector(".links");
+
+btn.addEventListener("click", function () {
+  links.classList.toggle("show");
+});
